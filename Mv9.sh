3155540a34940d526760f9b00f870e9c6b5099ae
abellardtadeo <55940693+abellardtadeo@users.noreply.github.com>
2019-10-06 20:07:59 +0700

    Add files via upload

---
+++ Mv9.sh
@@ -0,0 +1,386 @@
+figlet KAMI | lolcat
+figlet DARI | lolcat
+figlet MRTEAMZ  | lolcat
+figlet Termux  | lolcat
+
+
+
+
+		
+
+
+
+
+
+
+
+ 
+clear
+
+
+
+
+
+
+b="\033[1m"
+u="\033[4m"
+bl="\033[30m"
+r="\033[31m"
+g="\033[32m"
+bu="\033[34m"
+m="\033[35m"
+c="\033[36m"
+w="\033[37m"
+endc="\033[0m"
+enda="\033[0m"
+blue="\033[1;34m"
+cyan="\033[1;36m"
+red="\033[1;31m"
+
+figlet TOOLS v9 | lolcat
+
+echo "_____________________________________________________________"
+echo "Tools    : Kumpulan  Tools MrTEAMz TErMuX$white                         " |lolcat
+echo "Creadby  : MrTEAMz TErMuX $white                                      " |lolcat
+echo "Contact  : 081584172720   $white                             " |lolcat
+echo "_____________________________________________________________"
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo $red"[#]> (Ctrl + C ) Detected, Trying To Exit ... "
+echo $cyan"[#]> Thanks"
+sleep 1
+echo ""
+echo $white"[#]> see you gayn :)"
+sleep 1
+exit
+}
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+echo ""
+echo $r "00.  ABOUT${endc}";
+echo "============================" | lolcat
+echo $b "0.  install bahan bahan${enda}";
+echo "============================" | lolcat
+echo $b "1.  hack fb v1${enda}";
+echo "============================" | lolcat
+echo $r "2.  hack fb v2${endc}";
+echo "============================" | lolcat
+echo $g "3.  hack fb v3${endc}";
+echo "============================" | lolcat
+echo $c "4   spam Tol 1${endc}";
+echo "============================" | lolcat
+echo $r "5.  spam sms${endc}";
+echo "============================" | lolcat
+echo $r "6.  Tools-Dicky${endc}";
+echo "============================" | lolcat
+echo $r "7.  *Tools-B4J1N64Nv5*${endc}";
+echo "============================" | lolcat
+echo $r "8.  sadap whatsapp${endc}";
+echo "============================" | lolcat
+echo $r "9.  Tools lengkap${endc}";
+echo "============================" | lolcat
+echo $r "10.  hack Fb v4${endc}";
+echo "============================" | lolcat
+echo $r "11.  DDOS ATTACK HAMMER${endc}";
+echo "============================" | lolcat
+echo $r "12.  Spam sms${endc}";
+echo "============================" | lolcat
+echo $r "13.  BOM SMS${endc}";
+echo "============================" | lolcat
+echo $r "14.  install metasploit${endc}";
+echo "============================" | lolcat
+echo $r "15.  Tools Hack Bae By dicky${endc}";
+echo "============================" | lolcat
+echo $r "16.  Tools Hack fb v5${endc}";
+echo "============================" | lolcat
+echo $r "17.  install linux and install root 200mb${endc}";
+echo "============================" | lolcat
+echo $r "18.  54 TOOLS${endc}";
+echo "============================" | lolcat
+echo $r "19.  94 TOOLS${endc}";
+echo "============================" | lolcat
+echo $r "20.  311 TOOLS${endc}";
+echo "============================" | lolcat
+echo $r "21.  400+ TOOLS${endc}";
+echo "============================" | lolcat
+echo $r "22.  200+ TOOLS${endc}";
+echo "============================" | lolcat
+echo $r "23.  BOM${endc}";
+echo "============================" | lolcat
+echo $r "24.  HACK WIFI${endc}";
+echo "============================" | lolcat
+echo $r "25.  install trojans${endc}";
+echo "============================" | lolcat
+echo $r "99. Exit${endc}";
+echo ""
+echo "╭─PILIH" |lolcat
+read -p "╰─#" pil;
+
+# Nmap
+
+case $pil in
+
+
+00)echo "SCRIPT BY MRTEAMZ" | lolcat
+echo "LIST TEAM: " | lolcat
+echo "- MR ANSORI  TREND" | lolcat
+echo "-MR ABELLARD" | lolcat
+echo "-MR X~Sword" | lolcat
+echo "-Mr.X" | lolcat
+
+;;
+
+
+0) pkg update
+ pkg upgrade
+ pkg install php
+ pkg install openssh
+ pkg install toilet
+ pkg install bash
+ pkg install gem
+ pkg install ruby
+ pkg install git
+ apt update
+ apt upgrade
+ apt install git
+ pkg install linux
+ pkg install ruby
+ pkg install nmap
+ apt install nmap
+ pip2 install mechanize
+ pip2 install requests
+ pkg install vim
+ gem install lolcat
+ pkg install cowsay
+ pkg install neofetch
+echo "jika bahan sudah di download sekarang kalian pilih yang mana tools yang kamu ingin jalankan" | lolcat
+
+
+;;
+
+
+1) git clone https://github.com/arfan-05al/DarkFbVIP
+ cd DarkFbVIP
+ python2 DarkVIP.py
+ 
+
+;;
+
+
+
+2) git clone https://github.com/FR13ND8/BRUTEFORCEnew
+cd BRUTEFORCEnew
+sh new.sh
+
+;;
+
+
+
+3) git clone http://github.com/pirmansx/mbf
+ cd mbf
+ python2 MBF.py
+
+;;
+
+
+
+4) https://github.com/GUNAWAN18ID/B3604-spam-v1.git
+ cd B3604-spam-v1
+ bash B3604-spam.sh
+
+;;
+
+
+
+5) https://github.com/Senitopeng/SpamSms.git
+ cd SpamSms
+ python2 mantan.py
+
+;;
+
+6) git clone https://github.com/DICKYalone/dicky
+ cd dicky
+ sh Dickygt.sh
+ 
+ ;;
+ 
+7) git clone https://github.com/DarknessCyberTeam/B4J1N64Nv5
+ cd B4J1N64Nv5
+ sh B4J1N64N.sh
+ 
+ ;;
+ 
+8) git clone https://github.com/MrTamfanX/MrSadapWhatsApp
+ cd MrSadapWhatsApp
+ ls
+ cdmod +x*
+ ls
+ sh MrSadapWhatsApp.sh
+ 
+ ;;
+
+
+9) git clone https://github.com/rahmadxyz/ToolsLengkap
+ cd ToolsLengkap
+ sh rahmad.sh
+
+;;
+
+
+10) git clone https://github.com/FR13ND8/BruteFb
+ ls
+ cd bruteFb
+ ls
+ python2 brute.py
+ 
+ ;;
+ 
+ 
+ 11) git clone https://github.com/cyweb/hammer
+ cd hammer
+ chmod +x hammer.py
+ python hammer.py
+ 
+ ;;
+ 
+ 12)  git clone https://github.com/rahmadxyz/ToolsLengkap
+ cd ToolsLengkap
+ sh rahmad.sh
+ 
+ ;;
+ 
+ 
+ 13) git clone https://github.com/Amriez/gcospam
+ cd gcospam
+ sh install.sh
+ sh gco.sh
+ 
+ 
+ 
+ ;;
+ 
+ 
+ 14) pkg install root-repo
+ pkg install unstable-repo
+ pkg install x11-repo
+ pkg install metasploit
+ pkg install pv
+ 
+ 
+ 
+ ;;
+
+
+15) git clone https://github.com/mrcakil/Mrcakil
+ cd Mrcakil
+ chmod 777 tools2
+ ./tools2
+
+;;
+
+
+16) git clone https://github.com/essachriatian/premiumfbcrack
+ cd premiumfbcrack
+ sh tool.sh
+ 
+ 
+ ;;
+ 
+ 
+ 
+ 17) pkg update && pkg upgrade
+ pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh
+ ./start-kali.sh
+ 
+ ;;
+ 
+ 
+ 18) git clone https://github.com/Rusmana-ID/rus
+ cd rus
+ sh v2.sh
+ 
+ 
+ ;;
+ 
+ 19) git clone https://github.com/BanditSploit011/ProjectBandit
+ cd ProjectBandit
+ termux-fix-shebang BanditSploit.sh
+ sh BanditSploit.sh
+
+
+ ;;
+ 
+ 20) git clone https://github.com/TUANB4DUT/TOOLSINSTALLERV3
+ cd TOOLSINSTALLERV3
+ chmod +x TUANB4DUT.sh
+ sh TUANB4DUT.sh
+ 
+ ;;
+ 
+ 
+ 21) git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+ cd TOOLSINSTALLERv4
+ sh TUANB4DUT..sh
+ 
+ 
+ ;;
+ 
+ 
+ 22) git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+ cd BAJINGANv6
+ sh BAJINGAN.sh
+ 
+ 
+ ;;
+ 
+ 
+ 23) git clone https://github.com/Syhrularv/bomber
+ cd bomber
+ bash bom
+ 
+ ;;
+
+
+24) git clone https://github.com/escOrtd3w/wifi-hacker
+cd wifi-hacker
+chmod +x wifi-hacker.sh
+wifi-hacker.sh
+
+
+;;
+
+
+
+25) apt update
+apt upgrade
+pkg install python2
+pip2 install mechanize
+pip2 install request
+pkg install git
+git clone git clone https://github.com/R133F/Trojans
+cd Trojans
+python2 trojans.py
+
+
+
+
+
+;;
+
+
+
+99) echo "created by : ABELLARD" | lolcat
+exit
+;;
+
+*) echo "sorry, pilihan yang kamu cari tidak tersedia"
+esac
+done
+done
